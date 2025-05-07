1cc32b148654a97cbfe486ebb7ffa8bd9579a8d8
chrzzze <cherizze.aranzaso@gmail.com>
2025-05-07 18:25:32 +0800

    dhfg

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
@@ -9,9 +9,16 @@ public class Main {
             FileReader reader = new FileReader(subjectstxt.getPath());
             BufferedReader bufferedReader = new BufferedReader(reader);
 
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+            String subjects = bufferedReader.readLine();
+            System.out.println("Subjects: " + subjects);
+
+            // Split the string using comma as delimiter
+            String[] subs = subjects.split(", ");
+
+            // Display each element of the array
+            for (int i = 0; i < subs.length; i++) {
+                System.out.println("Subject " + (i+1) + ": " + subs[i]);
+
             }
             reader.close();
         } catch (IOException e) {
--- Student.java
+++ Student.java
@@ -1,8 +1,41 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Student {
     String name, subject;
     Double grade;
 
     public Student(String name, String subject, Double grade) {
+        try {
+            File subjectstxt = new File("data//subjects.txt");
+            FileReader reader = new FileReader(subjectstxt.getPath());
+            BufferedReader bufferedReader = new BufferedReader(reader);
+
+            String subjects = bufferedReader.readLine();
+            System.out.println("Subjects: " + subjects);
+
+            // Split the string using comma as delimiter
+            String[] subs = subjects.split(", ");
+
+            // Display each element of the array
+            for (int i = 0; i < subs.length; i++) {
+                System.out.println("Subject " + (i+1) + ": " + subs[i]);
+
+            }
+
+            for (int i = 0; i < subs.length) {
+                
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("you did it wrong");
+        }
+
+
         this.name = name;
         this.subject = subject;
         this.grade = grade;
