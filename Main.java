5babc33c553d3354779681f6a2defa13cc6ccf8a
Redd <reddgabrinao@gmail.com>
2025-05-15 21:14:18 +0800

    Oops I forgot to get rid of the decoy text

--- Main.java
+++ Main.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-       
-        File subjectstxt = new File("data//subjects.txt");
-//        DataManagement.write(subjectstxt.getPath(),  "test test ahhhhhh"); //testing lang toh, the file is DataManagement.java
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter username: ");
@@ -21,20 +18,6 @@ public class Main {
             System.out.println("Logged in as: " + currentUser.username);
         }
        
-       
-       
-        // try {
-        //     File subjectstxt = new File("data//subjects.txt");
-        //     FileReader reader = new FileReader(subjectstxt.getPath());
-        //     BufferedReader bufferedReader = new BufferedReader(reader);
 
-        //     String line;
-        //     while ((line = bufferedReader.readLine()) != null) {
-        //         System.out.println(line);
-        //     }
-        //     reader.close();
-        // } catch (IOException e) {
-        //     System.out.println("you did it wrong");
-        // }
     }
 }
